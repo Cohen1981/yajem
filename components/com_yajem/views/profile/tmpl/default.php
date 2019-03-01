04c7fffdde64d5ebe70b951f2ffb282d15d3934a
Alexander Bahlo <abahlo@hotmail.de>
2019-03-01 10:05:30 +0100

    Attendants in Profile

--- components/com_yajem/views/profile/tmpl/default.php
+++ components/com_yajem/views/profile/tmpl/default.php
@@ -39,7 +39,7 @@ $userId = $user->get('id');
 
 	<h2><?php echo Text::_('COM_YAJEM_CONTACT_DETAIL'); ?></h2>
 
-	<div class="yajem_grid_section">
+	<div class="yajem_grid_section yajem_bordered yajem_padding">
 
 		<div>
 			<?php echo Text::_('COM_YAJEM_NAME'); ?>
@@ -94,25 +94,62 @@ $userId = $user->get('id');
 
 	<h2><?php echo Text::_('COM_YAJEM_EQUIPMENT'); ?></h2>
 
-	<div class="yajem_grid_section">
+	<div class="yajem_grid_section yajem_bordered yajem_padding">
 		<div>
-            <b><?php echo Text::_('COM_YAJEM_EQUIPMENT_DESC'); ?></b>
-        </div>
+			<b><?php echo Text::_('COM_YAJEM_EQUIPMENT_DESC'); ?></b>
+		</div>
 		<div>
 			<b><?php echo Text::_('COM_YAJEM_EQUIPMENT_AREA'); ?></b>
-        </div>
-        <?php if($profile->equipmentItems): ?>
-            <?php foreach ($profile->equipmentItems as $item) : ?>
-            <?php $item = EquipmentItem::cast($item); ?>
-                <div>
-                    <?php echo $item->type; ?><br/>
-                    <?php echo $item->detail; ?>
-                </div>
-                <div>
-                    <?php echo $item->length . " x " . $item->width; ?>
-                </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
+		</div>
+		<?php if($profile->equipmentItems): ?>
+			<?php foreach ($profile->equipmentItems as $item) : ?>
+			<?php $item = EquipmentItem::cast($item); ?>
+				<div>
+					<?php echo $item->type; ?><br/>
+					<?php echo $item->detail; ?>
+				</div>
+				<div>
+					<?php echo $item->length . " x " . $item->width; ?>
+				</div>
+			<?php endforeach; ?>
+		<?php endif; ?>
+	</div>
+
+	<h2><?php echo Text::_('COM_YAJEM_ATTENDET'); ?></h2>
+
+	<div class="yajem_grid_section yajem_bordered yajem_padding">
+		<?php
+		if ($attendants = $profile->getAttendants())
+		{
+			foreach ($attendants as $attendet)
+			{
+				switch ($attendet->status)
+				{
+					case 0:
+						$userStatus = '<div> <i class="fas fa-question-circle" aria-hidden="true"> </i> ' .
+							Text::_("COM_YAJEM_NOT_DECIDED") . '</div>';
+						break;
+					case 1:
+						$userStatus = '<div> <i class="far fa-thumbs-up" aria-hidden="true"> </i> ' .
+							Text::_("COM_YAJEM_ATTENDING") . '</div>';
+						break;
+					case 2:
+						$userStatus = '<div> <i class="far fa-thumbs-down" aria-hidden="true"> </i> ' .
+							Text::_("COM_YAJEM_NOT_ATTENDING") . '</div>';
+						break;
+					case 3:
+						$userStatus = '<div> <i class="far fa-clock" aria-hidden="true"> </i> ' .
+							Text::_("COM_YAJEM_ON_WAITINGLIST") . '</div>';
+						break;
+				}
+
+				echo "<div><a href=\"" .
+					JRoute::_('index.php?option=com_yajem&task=event.view&id=' . (int) $attendet->eventId) . "\">" .
+					$attendet->event . "</a></div>";
+				echo $userStatus;
+			}
+		}
+		?>
 	</div>
 
 </div>
