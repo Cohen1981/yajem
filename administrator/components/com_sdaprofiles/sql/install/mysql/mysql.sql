870fefb0bae16f5ca0c54d37e69e2dfb7a4ff90b
Alexander Bahlo <abahlo@hotmail.de>
2019-03-20 13:01:03 +0100

    Understanding FOF

--- administrator/components/com_sdaprofiles/sql/install/mysql/mysql.sql
+++ administrator/components/com_sdaprofiles/sql/install/mysql/mysql.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `#__sdaprofiles_profiles`
 (
-  `profileId` int unsigned auto_increment,
-  `usersUserId` int unsigned comment 'foreign key to joomla user #__users',
+  `sdaprofiles_profile_id` int unsigned auto_increment,
+  `users_user_id` int unsigned comment 'foreign key to joomla user #__users',
   `avatar` varchar(255) null,
   `address1` varchar(255) null,
   `address2` varchar(255) null,
@@ -10,20 +10,34 @@ CREATE TABLE IF NOT EXISTS `#__sdaprofiles_profiles`
   `phone` varchar(20) null,
   `mobil` varchar(20) null,
   `dob` DATE null comment 'Date of birth',
-  PRIMARY KEY (`profileId`)
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  `created_by` bigint(20) NOT NULL DEFAULT '0',
+  `created_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` bigint(20) NOT NULL DEFAULT '0',
+  `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `locked_by` bigint(20) NOT NULL DEFAULT '0',
+  `locked_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`sdaprofiles_profile_id`)
 )
   ENGINE=InnoDB
   DEFAULT CHARSET=`utf8mb4` DEFAULT COLLATE=`utf8mb4_unicode_ci`;
 
 CREATE TABLE IF NOT EXISTS `#__sdaprofiles_fittings`
 (
-  `fittingId` int unsigned auto_increment comment 'Primary Key',
-  `sdaprofilesProfileId` int unsigned not null comment 'foreign key to #__sdaprofiles_profiles',
+  `sdaprofiles_fitting_id` int unsigned auto_increment comment 'Primary Key',
+  `sdaprofiles_profile_id` int unsigned not null comment 'foreign key to #__sdaprofiles_profiles',
   `type` varchar(255) null,
   `detail` varchar(255) null,
   `lenght` int null,
   `width` int null,
-  PRIMARY KEY (`fittingId`)
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  `created_by` bigint(20) NOT NULL DEFAULT '0',
+  `created_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` bigint(20) NOT NULL DEFAULT '0',
+  `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `locked_by` bigint(20) NOT NULL DEFAULT '0',
+  `locked_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`sdaprofiles_fitting_id`)
 )
   ENGINE=InnoDB
   DEFAULT CHARSET=`utf8mb4` DEFAULT COLLATE=`utf8mb4_unicode_ci`;
