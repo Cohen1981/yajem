eda4647e5ce732caed92d177163396eb3bd91137
Alexander Bahlo <abahlo@hotmail.de>
2019-04-03 09:28:15 +0200

    separating build files for components and package

--- administrator/components/com_sdajem/sql/uninstall/mysql/mysql.sql
+++ administrator/components/com_sdajem/sql/uninstall/mysql/mysql.sql
@@ -1,5 +1,6 @@
-DROP TABLE IF EXISTS `#__yajem_locations`;
-DROP TABLE IF EXISTS `#__yajem_events`;
-DROP TABLE IF EXISTS `#__yajem_attendees`;
-DROP TABLE IF EXISTS `#__yajem_comments`;
-DROP TABLE IF EXISTS `#__yajem_attachments`;
+DROP TABLE IF EXISTS `#__sdajem_locations`;
+DROP TABLE IF EXISTS `#__sdajem_events`;
+DROP TABLE IF EXISTS `#__sdajem_attendees`;
+DROP TABLE IF EXISTS `#__sdajem_comments`;
+DROP TABLE IF EXISTS `#__sdajem_attachments`;
+DROP TABLE IF EXISTS `#__sdajem_categories`;
