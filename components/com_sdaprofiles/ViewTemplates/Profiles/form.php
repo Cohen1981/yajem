4f7fb0332067b975993363bb8b1cb3e806d8bf15
Alexander Bahlo <abahlo@hotmail.de>
2019-04-08 13:58:03 +0200

    [#16] - Alerting before Save - prepare mailing

--- components/com_sdaprofiles/ViewTemplates/Profiles/form.php
+++ components/com_sdaprofiles/ViewTemplates/Profiles/form.php
@@ -8,18 +8,45 @@
  */
 
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Component\ComponentHelper;
 
 /** @var \Sda\Profiles\Site\View\Profile\Html   $this       */
 /** @var \Sda\Profiles\Site\Model\Profile       $profile    */
 /** @var \Sda\Profiles\Site\Model\Fitting       $fitting    */
 
 $this->addCssFile('media://com_sdaprofiles/css/style.css');
+$this->addJavascriptFile('media://com_sdaprofiles/js/profile.js');
 $profile = $this->getItem();
-echo $this->getRenderedForm();
+$params = ComponentHelper::getParams('com_sdaprofiles');
 
 ?>
+<div class="sdaprofiles_tabs">
+	<label id="basic_switch_label" class="sdaprofile_tab sda_active" for="basic_switch">
+		<?php echo Text::_('COM_SDAPROFILES_TITLE_PROFILES_READ') ?>
+	</label>
+	<label id="fitting_switch_label" class="sdaprofile_tab" for="fitting_switch">
+		<?php echo Text::_('COM_SDAPROFILES_TITLE_FITTING_BASIC') ?>
+	</label>
+	<label id="events_switch_label" class="sdaprofile_tab" for="events_switch">
+		<?php echo Text::_('COM_SDAJEM_EVENT_TITLE_LABEL') ?>
+	</label>
+	<?php if ((bool) $params->get('use_preferences')) : ?>
+	<label id="preferences_switch_label" class="sdaprofile_tab" for="preferences_switch">
+		<?php echo Text::_('') ?>
+	</label>
+	<?php endif;?>
+</div>
 
-<div class="sdaprofiles_fitting_container">
+<div>
+<input type="checkbox" id="basic_switch" class="sdaprofiles_hidden" hidden checked="checked" onchange="switchCheckBox('basic_switch')"/>
+<div class="sdaprofiles_switchable">
+	<?php echo $this->getRenderedForm(); ?>
+</div>
+</div>
+
+<div>
+<input type="checkbox" id="fitting_switch" class="sdaprofiles_hidden" hidden onchange="switchCheckBox('fitting_switch')"/>
+<div class="sdaprofiles_switchable">
 
 	<form name="fittingForm" id="fittingForm" class="form-horizontal">
 		<div class="well">
@@ -38,15 +65,19 @@ echo $this->getRenderedForm();
 
 		<div class="control-group ">
 			<label class="control-label " for="sdaprofiles_input_type">
-				<?php echo Text::_('COM_SDAPROFILES_FITTING_TYPE_LABEL'); ?>
+				<?php echo Text::_('COM_SDAPROFILES_FITTING_TYPE_LABEL') . "*"; ?>
 			</label>
 			<div class="controls">
-				<input id="sdaprofiles_input_type" type="text" name="type" value="" />
+				<input id="sdaprofiles_input_type" type="text" name="type" value="" required />
 				<span class="help-block">
 					<?php echo Text::_('COM_SDAPROFILES_FITTING_TYPE_DESC'); ?>
 				</span>
 			</div>
 		</div>
+		<input type="hidden"
+		       id="sdaprofilesTypeError"
+		       value="<?php echo Text::_('COM_SDAPROFILES_FITTING_TYPE_ERROR') ?>"
+		/>
 
 		<div class="control-group ">
 			<label class="control-label " for="sdaprofiles_input_type">
@@ -95,6 +126,12 @@ echo $this->getRenderedForm();
 	}
 	?>
 
+</div>
+</div>
+
+<div>
+<input type="checkbox" id="events_switch" class="sdaprofiles_hidden" hidden onchange="switchCheckBox('events_switch')"/>
+<div class="sdaprofiles_switchable">
 	<?php
 	if ($profile->attendees)
 	{
@@ -108,5 +145,14 @@ echo $this->getRenderedForm();
 		echo $this->loadAnyTemplate('site:com_sdajem/Events/organized');
 	}
 	?>
-
 </div>
+</div>
+
+<?php if ((bool) $params->get('use_preferences')) : ?>
+<div>
+	<input type="checkbox" id="preferences_switch" class="sdaprofiles_hidden" hidden onchange="switchCheckBox('preferences_switch')"/>
+	<div class="sdaprofiles_switchable">
+		<?php echo $this->loadAnyTemplate('site:com_sdaprofiles/Profiles/preferences'); ?>
+	</div>
+</div>
+<?php endif; ?>
--- media/com_sdaprofiles/js/fittings.js
+++ media/com_sdaprofiles/js/fittings.js
@@ -1,31 +1,34 @@
 function addFittingAjax() {
-	document.getElementById('sdaprofiles_fitting_add').hidden=true;
 	var formElement = document.querySelector("#fittingForm");
 	var formData = new FormData(formElement);
 
-	document.getElementById('sdaprofiles_input_type').value = "";
-	document.getElementById('sdaprofiles_input_detail').value = "";
-	document.getElementById('sdaprofiles_input_length').value = "";
-	document.getElementById('sdaprofiles_input_width').value = "";
+	if (document.getElementById('sdaprofiles_input_type').value != "") {
+		document.getElementById('sdaprofiles_fitting_add').hidden=true;
+		document.getElementById('sdaprofiles_input_type').value = "";
+		document.getElementById('sdaprofiles_input_detail').value = "";
+		document.getElementById('sdaprofiles_input_length').value = "";
+		document.getElementById('sdaprofiles_input_width').value = "";
 
-	var xhttp = new XMLHttpRequest();
-	xhttp.onload = function () {
-		if (this.readyState === 4 && this.status === 200) {
-			var html = xhttp.response;
-			if (html.toString() === "error")
-			{
-				alert('An Error occured');
+		var xhttp = new XMLHttpRequest();
+		xhttp.onload = function () {
+			if (this.readyState === 4 && this.status === 200) {
+				var html = xhttp.response;
+				if (html.toString() === "error") {
+					alert('An Error occured');
+				} else {
+					document.getElementById('fitting_area').innerHTML = document.getElementById('fitting_area').innerHTML + html;
+				}
+				document.getElementById('sdaprofiles_fitting_add').hidden = false;
 			}
-			else
-			{
-				document.getElementById('fitting_area').innerHTML = document.getElementById('fitting_area').innerHTML + html;
-			}
-			document.getElementById('sdaprofiles_fitting_add').hidden=false;
-		}
-	};
+		};
 
-	xhttp.open("POST", "index.php?option=com_sdaprofiles&view=Fittings&task=addFittingAjax");
-	xhttp.send(formData);
+		xhttp.open("POST", "index.php?option=com_sdaprofiles&view=Fittings&task=addFittingAjax");
+		xhttp.send(formData);
+	}
+	else {
+		document.getElementById('sdaprofiles_input_type').focus();
+		alert("Ausrüstungsart darf nicht leer sein");
+	}
 }
 
 function deleteFittingAjax(id) {
