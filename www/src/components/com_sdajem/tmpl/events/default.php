5892f5fa851fe8239828707d5bf7a32ccb6d558d
Alexander Bahlo <abahlo@hotmail.de>
2023-02-06 14:28:27 +0100

    Template angepasst und returnPage für die Veranstaltungliste gesetzt.

--- www/src/components/com_sdajem/tmpl/events/default.php
+++ www/src/components/com_sdajem/tmpl/events/default.php
@@ -27,20 +27,25 @@ $canDo = ContentHelper::getActions('com_sdajem');
 $user  = Factory::getApplication()->getIdentity();
 ?>
 
-<form action="<?= Route::_('index.php?option=com_sdajem&view=event') ?>" method="post" id="adminForm" name="adminForm">
-	<?= HTMLHelper::_('form.token'); ?>
-    <input type="hidden" name="option" value="com_sdajem">
-    <input type="hidden" name="task" value="" />
+<form action="<?php echo Route::_('index.php?option=com_sdajem'); ?>" method="post" name="adminForm" id="adminForm">
+    <input type="hidden" name="task" value=""/>
+    <input type="hidden" name="return" value="<?php echo $this->return_page; ?>"/>
+	<?php echo HTMLHelper::_('form.token'); ?>
     <div>
 	    <?php if ($canDo->get('core.create')) : ?>
-            <a href="<?php echo Route::_('index.php?option=com_sdajem&task=event.add') ?>">
-                <?php echo TEXT::_('COM_SDAJEM_EVENT_ADD') ?>
-            </a>
-            <a href="<?php echo Route::_('index.php?option=com_sdajem&task=location.add') ?>">
-			    <?php echo TEXT::_('COM_SDAJEM_LOCATION_ADD') ?>
-            </a>
+            <div class="mb-2">
+                <button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('event.add')">
+                    <span class="fas fa-check" aria-hidden="true"></span>
+				    <?php echo Text::_('COM_SDAJEM_EVENT_ADD'); ?>
+                </button>
+                <button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('location.add')">
+                    <span class="fas fa-check" aria-hidden="true"></span>
+				    <?php echo Text::_('COM_SDAJEM_LOCATION_ADD'); ?>
+                </button>
+            </div>
 	    <?php endif; ?>
     </div>
+</form>
 <?php foreach ($this->items as $i => $item) : ?>
 
 <div class="sda_row">
@@ -60,15 +65,24 @@ $user  = Factory::getApplication()->getIdentity();
                 ?>
             </a>
         </div>
+        <form action="<?php echo Route::_('index.php?option=com_sdajem&id=' . (int) $item->id); ?>"
+              method="post"
+              name="editForm<?php echo $i; ?>"
+              id="editForm<?php echo $i; ?>">
+            <input type="hidden" name="id" value="<?php echo $item->id; ?>">
         <div class="sda_icons">
 	        <?php if ($canDo->get('core.edit') || $canDo->get('core.edit.own')) : ?>
-                <a href="<?php echo Route::_('index.php?option=com_sdajem&task=event.edit&id=' . $item->id) ?>">EDIT</a>
+                <div class="icons">
+			        <?php echo HTMLHelper::_('eventicon.edit', $item, $params); ?>
+                </div>
+                <!--<a href="<?php /*echo Route::_('index.php?option=com_sdajem&task=event.edit&id=' . $item->id) */?>">EDIT</a> -->
 	        <?php endif; ?>
         </div>
+        </form>
     </div>
     <div>
         <?php echo $item->location_name ?>
     </div>
 </div>
 <?php endforeach; ?>
-</form>
+
