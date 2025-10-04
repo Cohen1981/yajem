d6124fdae165d2dd30591d5e3c6e3a476419efac
Alexander Bahlo <abahlo@hotmail.de>
2025-10-04 13:58:35 +0200

    New Docker setup

--- build_dev_env.sh
+++ build_dev_env.sh
@@ -1,20 +1,5 @@
-# create the joomla directory
-mkdir www/joomla
-# start docker containers
-docker-compose up -d
-# set priviliges
-chmod -Rf 755 *
-#download and unzip joomla
-cd www/joomla || exit
-curl -sSL https://downloads.joomla.org/cms/joomla4/4-2-6/Joomla_4-2-6-Stable-Full_Package.zip?format=zip > joomla.zip
-sudo apt install unzip
-unzip joomla.zip
-chmod -Rf 777 *
-#create symbolic links for own code
-ln -sr /home/joomla/development/yajem/www/src/administrator/components/com_sdajem /home/joomla/development/yajem/www/joomla/administrator/components/com_sdajem
-ln -sr /home/joomla/development/yajem/www/src/components/com_sdajem /home/joomla/development/yajem/www/joomla/components/com_sdajem
-ln -sr /home/joomla/development/yajem/www/src/templates/survivants /home/joomla/development/yajem/www/joomla/templates/survivants
-ln -sr /home/joomla/development/yajem/www/src/media/com_sdajem /home/joomla/development/yajem/www/joomla/media/com_sdajem
-ln -sr /home/joomla/development/yajem/www/src/media/templates/site/survivants /home/joomla/development/yajem/www/joomla/media/templates/site/survivants
-
-cd ../..
+ln -sr ./src/administrator/components/com_sdajem ./joomla_data/administrator/components/com_sdajem
+ln -sr ./src/components/com_sdajem ./joomla_data/components/com_sdajem
+ln -sr ./src/media/com_sdajem ./joomla_data/media/com_sdajem
+ln -sr ./src/media/templates/site/survivants ./joomla_data/media/templates/site/survivants
+ln -sr ./src/templates/survivants ./joomla_data/templates/survivants
