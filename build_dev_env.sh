98e048230ab83517b39c2eb239c043fc2f09d65b
Your Name <abahlo@hotmail.de>
2025-10-05 17:54:30 +0200

    cleanup_links

--- build_dev_env.sh
+++ build_dev_env.sh
@@ -1,5 +1,3 @@
-ln -sr ./src/administrator/components/com_sdajem ./joomla_data/administrator/components/com_sdajem
-ln -sr ./src/components/com_sdajem ./joomla_data/components/com_sdajem
-ln -sr ./src/media/com_sdajem ./joomla_data/media/com_sdajem
-ln -sr ./src/media/templates/site/survivants ./joomla_data/media/templates/site/survivants
-ln -sr ./src/templates/survivants ./joomla_data/templates/survivants
+apt install make
+usermod -aG docker $USER
+newgrp docker
